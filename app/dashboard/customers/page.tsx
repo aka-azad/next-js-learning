6ae33cdc1660000ab6c081863c409444f446a1c7
Tanjil Ahmed <129624325+tanjilahmed12345@users.noreply.github.com>
2025-03-10 13:03:13 +0600

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CustomersPage = () => {
+    return (
+        <div>
+            <p>customers page</p>
+        </div>
+    );
+};
+
+export default CustomersPage;
